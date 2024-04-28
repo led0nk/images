626f7831d0049a6b10c4c61e6534f90f392c5f05
Janik <10290002+led0nk@users.noreply.github.com>
2024-04-28 16:08:35 +0200

    switch to fedora-base-image
    fixing build-args
    
    script update & pkg's update
    
    silverblue-update: fixed wrong scriptpath
    
    fix BUILD-ARGS
    
    fixed typo
    
    fixed baseimg-typo
    
    silverblue-update
    
    some shellcheck fixes
    
    some functional fixes to post-install.sh
    
    podman-docker fix

--- var/opt/post-install.sh
+++ var/opt/post-install.sh
@@ -1,47 +1,108 @@
 #!/usr/bin/bash
 
-#set repository-path
-#export PATH=~/git/repo/
-export HELP_PATH=/var/home/$USER/git/repo/dotfiles
+# set path variables
 export REPO_PATH=/var/home/$USER/git/repo
+export DOT_PATH=$REPO_PATH/dotfiles
+export GITHUB=https://github.com/led0nk
 
-#create directories
-mkdir -p $REPO_PATH/{dotfiles,minecraft,images}
-mkdir -p $HOME/.config/{sway,waybar,hypr,rofi,wofi}
-mkdir -p $HOME/Pictures/Wallpaper/
+# set some colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
 
-#clone git repositories
-git clone https://github.com/led0nk/dotfiles.git $REPO_PATH/dotfiles
-git clone https://github.com/led0nk/minecraft.git $REPO_PATH/minecraft
-git clone https://github.com/led0nk/images.git $REPO_PATH/repo/images
+# check the existing symlinks
+check_symlink() {
+	tested_symlink="$1"
 
-#create systemlinks for dotfiles
-ln -s $HELP_PATH/zsh/.zshrc $HOME/.zshrc
-ln -s $HELP_PATH/zsh/.zshenv $HOME/.zshenv
-ln -s $HELP_PATH/zsh/.p10k.zsh $HOME/.p10k.zsh
-ln -s $HELP_PATH/gitconfig/.gitconfig $HOME/.gitconfig
-ln -s $HELP_PATH/.config/sway/config $HOME/.config/sway/config
-ln -s $HELP_PATH/.config/waybar/config.jsonc $HOME/.config/waybar/config.jsonc
-ln -s $HELP_PATH/.config/waybar/style.css $HOME/.config/waybar/style.css
-ln -s $HELP_PATH/.config/hypr/hyprland.conf $HOME/.config/hypr/hyprland.conf
-ln -s $HELP_PATH/.config/wofi/style.css $HOME/.config/wofi/style.css
+	if [ -L "$tested_symlink" ]; then
+		if [ ! -e "$tested_symlink" ]; then
+			echo "${RED}error${NC}: symlink for $tested_symlink is broken"
+		else
+			echo "${GREEN}checked ${NC}$tested_symlink"
+		fi
+	else
+		echo "${RED}error${NC}: no symlink for $tested_symlink existing"
+	fi
+}
 
-#copy themefiles for rofi and background
-cp -r $HELP_PATH/.config/themes $HOME/.config/rofi/
-cp -r $HELP_PATH/background.png $HOME/Pictures/Wallpaper/background.png
+# create symlink & test or abort if test fails
+symlink() {
+	linkto=$1
+	linkfrom=$2
 
-#link golang variable
-echo export PATH=$HELP_PATH:/usr/lib/golang/bin >$HOME/.profile
+	echo "creating symlink for $linkfrom:"
+	ln -s "$linkto" "$linkfrom" && check_symlink "$linkfrom" || abort_func "$linkfrom"
+}
 
-#install zplug + extensions + change shell to zsh
+# function for cloning git repo
+install_git_repo() {
+	repo=$1
+	target=$2
+
+	echo "cloning $repo into $target"
+	git clone "$GITHUB/$repo" "$target" || abort_func "$repo"
+	echo "cloning of $repo done"
+}
+
+# function for exiting after error
+abort_func() {
+	errorfunction=$1
+	echo "${RED}Error:${NC} exiting $errorfunction"
+	exit 1
+}
+
+# delete zsh config(from installation)
+rm -rf "$HOME"/.zshrc
+
+# create directories
+mkdir -p "$HOME"/.config/{swappy,sway,waybar,rofi}
+mkdir -p "$HOME"/Pictures/Wallpaper/
+
+# clone GitHub repositories
+install_git_repo dotfiles.git "$DOT_PATH"
+install_git_repo images.git "$REPO_PATH"/images
+
+# create symlinks for dotfiles
+symlink "$DOT_PATH"/zsh/.zshrc "$HOME"/.zshrc
+symlink "$DOT_PATH"/zsh/.zshenv "$HOME"/.zshenv
+symlink "$DOT_PATH"/zsh/.p10k.zsh "$HOME"/.p10k.zsh
+symlink "$DOT_PATH"/gitconfig/.gitconfig "$HOME"/.gitconfig
+symlink "$DOT_PATH"/.config/sway/config "$HOME"/.config/sway/config
+symlink "$DOT_PATH"/.config/waybar/config.jsonc "$HOME"/.config/waybar/config.jsonc
+symlink "$DOT_PATH"/.config/waybar/style.css "$HOME"/.config/waybar/style.css
+symlink "$DOT_PATH"/.config/nvim "$HOME"/.config/nvim
+symlink "$DOT_PATH"/.config/swappy/config "$HOME"/.config/swappy/config
+
+# copy themefiles and background
+cp -r "$DOT_PATH"/.config/rofi/themes "$HOME"/.config/rofi/ || abort_func "copying rofi themes"
+cp -r "$DOT_PATH"/background.png "$HOME"/Pictures/Wallpaper/background.png || abort_func "copying wallpaper"
+
+# link golang variable
+echo export PATH="$PATH":/usr/lib/golang/bin >>"$HOME"/.profile
+
+# install zplug + extensions + change shell to zsh
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 chsh -s /usr/bin/zsh
 zsh
 zplug install
 
-#remove files necessary for post-install-script
-sudo rm -rf /var/opt/dotfiles
-sudo rm -f /etc/systemd/system/firstboot.service
+while getopts 'ynh' OPTION; do
+	case "$OPTION" in
+	y)
+		sudo cp -r "$DOT_PATH"/etc/systemd/system/dotfile.service /etc/systemd/system/dotfile.service
+		sudo chmod +x /etc/systemd/system/dotfile.service
+		;;
+	h)
+		echo "install-dotfiles.sh [-y] [-h help]"
+		echo "[-y]  -   includes systemd-service for automatic dotfiles-update at startup"
+		echo "[-n]  -   excludes systemd-service for automatic dotfiles-update at startup"
+		;;
+	?)
+		echo "install-dotfiles.sh [-y] [-n] [-h help]"
+		exit 1
+		;;
+	esac
+done
+shift "(($OPTION - 1))"
 
-#generating ssh-key with github alias-mail
-ssh-keygen -t ed25519 -C "10290002+led0nk@users.noreply.github.com"
+sudo rm -rf /etc/profile.d/post-install.sh
